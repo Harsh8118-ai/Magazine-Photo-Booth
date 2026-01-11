64e40bccb34324400f1fefddc722d982207d62c6
Harsh Tyagi <tyagih7654321@gmail.com>
2026-01-11 22:40:08 +0530

    Final Code

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -138,7 +138,7 @@ export default function Hero() {
                                 aria-label="View Full Gallery"
                             >
                                 View Gallery
-                            </Button3D>
+                            </Button3D> 
                         </a>
                     </div>
 
