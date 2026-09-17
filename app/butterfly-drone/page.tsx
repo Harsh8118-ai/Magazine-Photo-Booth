5389f9d98e55d2bd5df3276d766645b722450f75
Harsh Tyagi <tyagih7654321@gmail.com>
2026-09-17 18:12:13 +0530

    final code

--- app/butterfly-drone/page.tsx
+++ app/butterfly-drone/page.tsx
@@ -66,7 +66,7 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      "max-image-preview": "large",   
+      "max-image-preview": "large",      
     },
   },
 
